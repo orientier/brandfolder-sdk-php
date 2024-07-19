a349704ce682e3ad86676f10a5f1e811ebfede36
Nathanael Dewhurst <nathanael@orientier.com>
2024-07-19 14:50:20 -0400

    Remove superfluous use declarations.

--- src/Brandfolder.php
+++ src/Brandfolder.php
@@ -2,13 +2,6 @@
 
 namespace Brandfolder;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\InvalidArgumentException;
-use GuzzleHttp\Utils as GuzzleUtils;
-use stdClass;
-
 /**
  * Class Brandfolder.
  *
